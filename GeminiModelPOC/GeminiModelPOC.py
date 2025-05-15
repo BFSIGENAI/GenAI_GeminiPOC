98754d3940855d7f9fdbb6eecfba9a62aeb42edd
132120@TCSGENAILAB.onmicrosoft.com <132120@TCSGENAILAB.onmicrosoft.com>
2025-05-15 05:08:41 +0000

    commit

--- GeminiModelPOC/GeminiModelPOC.py
+++ GeminiModelPOC/GeminiModelPOC.py
@@ -9,6 +9,6 @@ GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
 genai.configure(api_key=GOOGLE_API_KEY)
 model = genai.GenerativeModel('gemini-2.0-flash')
 
-response = model.generate_content("how to file a legal case in consumer court")
+response = model.generate_content("how to motivate a 4 year kid to start writing")
 print(response.text)
 
